efadc9137f80265fc41b8d1e751fbda51b1fb8ed
Mathew Bramson <mathewbramson@gmail.com>
2017-11-05 16:06:26 -0500

    Set up project for hex

--- mix.exs
+++ mix.exs
@@ -6,8 +6,13 @@ defmodule OrderInvariantCompare.Mixfile do
       app: :order_invariant_compare,
       version: "1.0.0",
       elixir: "~> 1.3",
+      build_embedded: Mix.env == :prod,
       start_permanent: Mix.env == :prod,
-      deps: deps()
+      description: description(),
+      package: package(),
+      deps: deps(),
+      name: "OrderInvariantCompare",
+      source_url: "https://github.com/mbramson/order_invariant_compare"
     ]
   end
 
@@ -19,7 +24,25 @@ defmodule OrderInvariantCompare.Mixfile do
 
   defp deps do
     [
+      {:ex_doc, "~> 0.18", only: :dev},
       { :quixir, "~> 0.9", only: :test }
+    ]
+  end
+
+  defp description() do
+    """
+    OrderInvariantCompare provides functionality for order invariant equality
+    comparison.
+    """
+  end
+
+  defp package() do
+    [
+      name: "OrderInvariantCompare",
+      files: ["lib", "mix.exs", "README.md", "LICENSE.md"],
+      maintainers: ["Mathew Bramson"],
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/mbramson/order_invariant_compare"}
     ]
   end
 end
